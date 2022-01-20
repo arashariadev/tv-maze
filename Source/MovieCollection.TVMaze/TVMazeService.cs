16fea6a137f3f8ec37198da70b9147a19324fd9d
Arash Aria <aarash32322@gmail.com>
2022-01-21 02:31:37 +0300

    Remove the interface.

--- Source/MovieCollection.TVMaze/TVMazeService.cs
+++ Source/MovieCollection.TVMaze/TVMazeService.cs
@@ -12,7 +12,7 @@ namespace MovieCollection.TVMaze
     /// <summary>
     /// The <c>TVMazeService</c> class.
     /// </summary>
-    public class TVMazeService : ITVMazeService
+    public class TVMazeService
     {
         private readonly HttpClient _httpClient;
         private readonly TVMazeOptions _options;
@@ -22,7 +22,6 @@ namespace MovieCollection.TVMaze
         /// </summary>
         /// <param name="httpClient">An instance of <see cref="HttpClient"/>.</param>
         public TVMazeService(HttpClient httpClient)
-            : base()
         {
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
             _options = new TVMazeOptions();
@@ -34,13 +33,16 @@ namespace MovieCollection.TVMaze
         /// <param name="httpClient">An instance of <see cref="HttpClient"/>.</param>
         /// <param name="options">An instance of <see cref="TVMazeOptions"/>.</param>
         public TVMazeService(HttpClient httpClient, TVMazeOptions options)
-            : base()
         {
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
             _options = options ?? throw new ArgumentNullException(nameof(options));
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Search through all the shows in our database by the show's name.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Search>> SearchShowsAsync(string query)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -52,7 +54,15 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Search for a single tv show.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint allows embedding of additional information.
+        /// </remarks>
+        /// <param name="query">The search query.</param>
+        /// <param name="embed">The embeddings.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<Show> SearchSingleShowAsync(string query, params string[] embed)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -69,7 +79,11 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Search for a tv show with imdb id.
+        /// </summary>
+        /// <param name="imdbId">The imdb id of the show.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<Show> SearchByIMDbIdAsync(string imdbId)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -81,7 +95,11 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Search for a tv show with tvdb id.
+        /// </summary>
+        /// <param name="tvdbId">The tvdb id of the show.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<Show> SearchByTVDbIdAsync(string tvdbId)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -93,7 +111,11 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Search for a tv show with tvrage id.
+        /// </summary>
+        /// <param name="tvRageId">The tvrage id of the show.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<Show> SearchByTVRageIdAsync(string tvRageId)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -105,7 +127,11 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Search for people.
+        /// </summary>
+        /// <param name="query">The search query.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<SearchPerson>> SearchPeopleAsync(string query)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -117,7 +143,16 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// The schedule is a complete list of episodes that air in a given country on a given date.
+        /// Episodes are returned in the order in which they are aired, and full information about the episode and the corresponding show is included.
+        /// </summary>
+        /// <remarks>
+        /// Note that contrary to what you might expect, the ISO country code for the United Kingdom is not UK, but GB.
+        /// </remarks>
+        /// <param name="dateTime">The schedule date (defaults to the present day).</param>
+        /// <param name="country">The schedule country (defaults to "US").</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Schedule>> GetScheduleAsync(DateTime? dateTime = null, string country = null)
         {
             var parameters = new List<KeyValuePair<string, string>>();
@@ -138,7 +173,19 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// The web schedule is a complete list of episodes that air on web/streaming channels on a given date.
+        /// TVmaze distinguishes between local and global Web Channels: local Web Channels are only available in one
+        /// specific country, while global Web Channels are available in multiple countries.
+        /// </summary>
+        /// <remarks>
+        /// To query both local and global Web Channels, leave out the country parameter.
+        /// To query only local Web Channels, set country to an ISO country code.
+        /// And to query only global Web Channels, set country to an empty string.
+        /// </remarks>
+        /// <param name="dateTime">The schedule date (defaults to the present day).</param>
+        /// <param name="country">The schedule country (defaults to "US").</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Schedule>> GetStreamingScheduleAsync(DateTime? dateTime = null, string country = null)
         {
             var parameters = new List<KeyValuePair<string, string>>();
@@ -159,15 +206,28 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// The full schedule is a list of all future episodes known to TVmaze, regardless of their country.
+        /// Be advised that this endpoint's response is at least several MB large.
+        /// As opposed to the other endpoints, results are cached for 24 hours.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Schedule>> GetFullScheduleAsync()
         {
             return await GetJsonAsync<IList<Schedule>>("/schedule/full")
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
-        public async Task<Show> GetShowInfoAsync(int id, params string[] embed)
+        /// <summary>
+        /// Retrieve all primary information for a given show.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint allows embedding of additional information.
+        /// </remarks>
+        /// <param name="showId">The show id.</param>
+        /// <param name="embed">The embeddings.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task<Show> GetShowInfoAsync(int showId, params string[] embed)
         {
             var parameters = new List<KeyValuePair<string, string>>();
 
@@ -176,11 +236,18 @@ namespace MovieCollection.TVMaze
                 AddEmbeddedPrametersToList(embed, parameters);
             }
 
-            return await GetJsonAsync<Show>($"/shows/{id}", parameters)
+            return await GetJsonAsync<Show>($"/shows/{showId}", parameters)
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A complete list of episodes for the given show.
+        /// Episodes are returned in their airing order, and include full episode information.
+        /// By default, specials are not included in the list.
+        /// </summary>
+        /// <param name="showId">The show id.</param>
+        /// <param name="specials">Should specials be included.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Episode>> GetShowEpisodesListAsync(int showId, bool specials = false)
         {
             var parameters = new List<KeyValuePair<string, string>>();
@@ -194,7 +261,14 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Retrieve one specific episode from this show given its season number and episode number.
+        /// This either returns the full information for one episode, or a HTTP 404.
+        /// </summary>
+        /// <param name="showId">The show id.</param>
+        /// <param name="season">The season number.</param>
+        /// <param name="episode">The episode number.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<Episode> GetShowEpisodeAsync(int showId, int season, int episode)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -207,7 +281,14 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Retrieve all episodes from this show that have aired on a specific date.
+        /// This either returns an array of full episode info, or a HTTP 404.
+        /// Useful for daily (talk) shows that don't adhere to a common season numbering.
+        /// </summary>
+        /// <param name="showId">The show id.</param>
+        /// <param name="dateTime">The episode date.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Episode>> GetShowEpisodesByDateAsync(int showId, DateTime dateTime)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -219,42 +300,80 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A complete list of seasons for the given show.
+        /// Seasons are returned in ascending order and contain the full information that's known about them.
+        /// </summary>
+        /// <param name="showId">The show id.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Season>> GetShowSeasonsAsync(int showId)
         {
             return await GetJsonAsync<IList<Season>>($"/shows/{showId}/seasons")
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A list of episodes in this season. Specials are always included in this list;
+        /// they can be recognized by a NULL value for number.
+        /// </summary>
+        /// <param name="seasonId">The season id.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Episode>> GetSeasonEpisodesAsync(int seasonId)
         {
             return await GetJsonAsync<IList<Episode>>($"/seasons/{seasonId}/episodes")
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A list of main cast for a show.
+        /// Each cast item is a combination of a person and a character.
+        /// Items are ordered by importance, which is determined by the total number of appearances of the given character in this show.
+        /// </summary>
+        /// <param name="showId">The show id.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Cast>> GetShowCastAsync(int showId)
         {
             return await GetJsonAsync<IList<Cast>>($"/shows/{showId}/cast")
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A list of main crew for a show.
+        /// Each crew item is a combination of a person and their crew type.
+        /// </summary>
+        /// <param name="showId">The show id.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Crew>> GetShowCrewAsync(int showId)
         {
             return await GetJsonAsync<IList<Crew>>($"/shows/{showId}/crew")
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A list of AKA's (aliases) for a show. An AKA with its country set to null indicates an AKA in the show's original country.
+        /// Otherwise, it's the AKA for that show in the given foreign country.
+        /// </summary>
+        /// <param name="showId">The show id.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<ShowAlias>> GetShowAliasesAsync(int showId)
         {
             return await GetJsonAsync<IList<ShowAlias>>($"/shows/{showId}/akas")
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// A list of all shows in our database, with all primary information included.
+        /// You can use this endpoint for example if you want to build a local cache of all shows contained in the TVmaze database.
+        /// This endpoint is paginated, with a maximum of 250 results per page.
+        /// The pagination is based on show ID, e.g. page 0 will contain shows with IDs between 0 and 250.
+        /// This means a single page might contain less than 250 results, in case of deletions, but it also guarantees that deletions won't cause shuffling in the page numbering for other shows.
+        /// Because of this, you can implement a daily/weekly sync simply by starting at the page number where you last left off, and be sure you won't skip over any entries.
+        /// For example, if the last show in your local cache has an ID of 1800, you would start the re-sync at page number floor(1800/250) = 7.
+        /// After this, simply increment the page number by 1 until you receive a HTTP 404 response code, which indicates that you've reached the end of the list.
+        /// As opposed to the other endpoints, results from the show index are cached for up to 24 hours.
+        /// </summary>
+        /// <param name="page">The page number.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<Show>> GetShowIndexAsync(int page = 1)
         {
             var parameters = new List<KeyValuePair<string, string>>()
@@ -266,7 +385,13 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Retrieve all primary information for a given episode.
+        /// This endpoint allows embedding of additional information.
+        /// </summary>
+        /// <param name="episodeId">The episode id.</param>
+        /// <param name="embed">The embeddings.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<Episode> GetEpisodeByIdAsync(int episodeId, params string[] embed)
         {
             var parameters = new List<KeyValuePair<string, string>>();
@@ -280,7 +405,13 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Retrieve all primary information for a given person.
+        /// This endpoint allows embedding of additional information.
+        /// </summary>
+        /// <param name="personId">The person id.</param>
+        /// <param name="embed">The embeddings.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<Person> GetPersonInfoAsync(int personId, params string[] embed)
         {
             var parameters = new List<KeyValuePair<string, string>>();
@@ -294,7 +425,15 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Retrieve all (show-level) cast credits for a person.
+        /// A cast credit is a combination of both a show and a character.
+        /// By default, only a reference to each show and character will be returned.
+        /// However, this endpoint supports embedding, which means full information for the shows and characters can be included.
+        /// </summary>
+        /// <param name="personId">The person id.</param>
+        /// <param name="embed">The embeddings.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<CastCredits>> GetCastCreditsAsync(int personId, params string[] embed)
         {
             var parameters = new List<KeyValuePair<string, string>>();
@@ -308,7 +447,15 @@ namespace MovieCollection.TVMaze
                 .ConfigureAwait(false);
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Retrieve all (show-level) crew credits for a person.
+        /// A crew credit is combination of both a show and a crew type.
+        /// By default, only a reference to each show will be returned.
+        /// However, this endpoint supports embedding, which means full information for the shows can be included.
+        /// </summary>
+        /// <param name="personId">The person id.</param>
+        /// <param name="embed">The embeddings.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task<IList<CrewCredits>> GetCrewCreditsAsync(int personId, params string[] embed)
         {
             var parameters = new List<KeyValuePair<string, string>>();
